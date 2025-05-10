fb51273023c22c046b82707b95b664a1813b16be
anniehernandez <sofia.hernandezslz@uanl.edu.mx>
2025-05-10 14:07:35 -0600

    Creation of Header component

---
+++ AeolusVr/Frontend/src/Components/Header.jsx
@@ -0,0 +1,26 @@
+import HyperViewLogo from '../assets/HyperViewLogo.png'
+import HyperViewLogo2 from '../assets/HyperViewLogo2.png'
+
+function Header (){
+    return(
+        <>
+            <div className="flex flex-row justify-between items-center fixed top-0 left-0 right-0 bg-gradient-to-b from-[var(--background-color)] to-[var(--gradient-color)] mix-blend-multiply py-3 px-30">
+                <img src={HyperViewLogo} alt="HyperView" className='h-10 cursor-pointer'/>
+            </div>
+            <div className='flex flex-row justify-between items-center fixed top-0 left-0 right-0 py-3 px-30'>
+
+              <img src={HyperViewLogo} alt="HyperView" className='h-10 cursor-pointer'/>
+
+                <div className='flex items-center justify-center text-sm text-regular'>
+                    <ol className='flex flex-row mx-10'>
+                        <li className="relative mx-10 cursor-pointer hover:underline">About Us</li>
+                        <li className='relative mx-10 cursor-pointer hover:underline'>Projects</li>
+                        <li className='relative mx-10 cursor-pointer hover:underline'>Sign Up</li>
+                        {/* <li className="relative mx-10 cursor-pointer after:absolute after:left-0 after:bottom-0 after:h-[1px] after:bg-white after:w-0 after:transition-all after:duration-300 after:ease-out hover:after:w-full">About Us</li> */}
+                    </ol>
+                    <img src={HyperViewLogo2} alt="HyperView" className='h-10'/>
+                </div>
+            </div>
+        </>
+    )
+}export default Header;
