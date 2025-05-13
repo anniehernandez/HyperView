54ca3c59ab59fef9b166b714eaa06a6fe098c246
anniehernandez <sofia.hernandezslz@uanl.edu.mx>
2025-05-13 11:20:37 -0600

    Added routes for pages

--- AeolusVr/Frontend/src/App.jsx
+++ AeolusVr/Frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './Pages/Home';
+import About_Us from './Pages/About_Us';
+import Aeolus_Vr from './Pages/Aeolus_Vr';
+import Sign_Up from './Pages/Sign_Up';
 import './App.css'
 
 function App() {
@@ -8,6 +11,9 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="/About_Us" element={<About_Us/>}/>
+          <Route path="/Projects/Aeolus_Vr" element={<Aeolus_Vr/>}/>
+          <Route path="/Sign_Up" element={<Sign_Up/>}/>
         </Routes>
       </Router>
     </>
