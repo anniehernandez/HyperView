ba8a767e6ef80dfa360d98ebae934674037f1749
anniehernandez <sofia.hernandezslz@uanl.edu.mx>
2025-05-13 22:25:41 -0600

    Added Contact Us component

--- AeolusVr/Frontend/src/Pages/Home.jsx
+++ AeolusVr/Frontend/src/Pages/Home.jsx
@@ -1,11 +1,11 @@
 import HyperViewLogo from '../assets/HyperViewLogo.png';
 import AeolusVrLogo from '../assets/AeolusVrLogo.png';
-import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
-import EmojiObjectsOutlinedIcon from '@mui/icons-material/EmojiObjectsOutlined';
 import Windows from '../assets/windows.jpg';
 import Rectangle from '../assets/Rectangle.png';
+import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
+import EmojiObjectsOutlinedIcon from '@mui/icons-material/EmojiObjectsOutlined';
 import Header from '../Components/Header.jsx';
-import Footer from '../Components/Footer.jsx';
+import Contact_Us from '../Components/Contact_Us.jsx';
 
 function Home(){
     return(
@@ -19,18 +19,17 @@ function Home(){
                         backgroundImage: `linear-gradient(to bottom, rgba(13, 42, 84, 0.8), rgba(13, 42, 84, 0) 100%), url(${Windows})` 
                     }}
                 >
-                    <h3 className="text-4xl font-semibold text-white">Technology for the real world.</h3>
+                    <h3 className="text-4xl font-semibold">Technology for the real world.</h3>
                     <img src={HyperViewLogo} alt="Logo" className="w-1/12 m-2"/>
                 </div>
 
-                {/* WHO ARE WE */}
                 <div className="flex flex-row justify-center items-center h-full">
                     <div className="flex flex-row w-3/4 h-auto bg-gray-200 rounded-bl-[3vw] rounded-tr-[3vw] shadow-lg ">
                         <div className="p-20">
                             <h2 className="text-4xl font-bold text-blue-950">Who are we?</h2>
                             <p className="text-base text-black my-10">
-                            We are a technology, innovation, and research oriented company dedicated to provide and develop the most creative, 
-                            technological and innovating solutions for a variety of fields including Education, World exploration, Investigation etc.
+                                We are a technology, innovation, and research oriented company dedicated to provide and develop the most creative, 
+                                technological and innovating solutions for a variety of fields including Education, World exploration, Investigation etc.
                                 <br /><br />
                                 Our goal is to use technology as a bridge between simulation and reality, creating effective tools for overcoming phobias, 
                                 specialized training, and rehabilitation in various areas.
@@ -79,39 +78,7 @@ function Home(){
                 </div>
             </div>
 
-            <div className='h-screen flex flex-col justify-between'>
-                <div className='flex flex-row justify-center bg-linear-to-b from-blue-950 from-30% to-blue-900 h-full p-10'>
-                    <div className='justify-start max-w-1/4 mx-10 my-30'>
-                        <h2 className='text-4xl font-bold'>Contact Us</h2>
-                        <p className='text-xl m-2'>we’ll get in touch with you as soon as possible!</p>
-                    </div>
-                    <div className='flex flex-col justify-center items-center bg-[var(--background-color)]/50 rounded-[3vw] w-1/4 m-10 py-10 px-20 '>
-                        <img src={HyperViewLogo} alt="" className='h-10'/>
-
-                        <div className='my-5 w-full'>
-                            <div className='my-4'>
-                                <h3 className='font-bold'>Full name</h3>
-                                <input type="text" placeholder='Full Name' className='w-full border-b-2 border-blue-500/70 p-1 focus:outline-0 focus:border-blue-500'/>
-                            </div>
-
-                            <div className='my-4'>
-                                <h3 className='font-bold'>Email</h3>
-                                <input type="text" placeholder='Email' className='w-full border-b-2 border-blue-500/70 p-1 focus:outline-0 focus:border-blue-500'/>
-                            </div>
-                            
-                            <div className='my-4'>
-                                <h3 className='font-bold'>Message</h3>
-                                <textarea type="text" className='w-full border-b-2 border-blue-500/70 p-1 focus:outline-0 focus:border-blue-500'/>
-                            </div>
-                        </div>
-
-                        <button className="bg-blue-500 font-bold rounded-2xl cursor-pointer w-full py-2 m-2 hover:bg-blue-400 focus:outline-2 focus:outline-offset-2 focus:outline-blue-500 active:bg-blue-600 ">
-                            Contact Us
-                        </button>
-                    </div>
-                </div>
-                <Footer/>
-            </div>
+            <Contact_Us/>
         </>
     )
 }export default Home;
