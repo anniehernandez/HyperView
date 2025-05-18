07984af7295294cab8a22525032e2aa1b712c945
anniehernandez <sofia.hernandezslz@uanl.edu.mx>
2025-05-18 01:36:13 -0600

    Added form for messages

--- AeolusVr/Frontend/src/Components/Contact_Us.jsx
+++ AeolusVr/Frontend/src/Components/Contact_Us.jsx
@@ -1,7 +1,80 @@
+import { useState } from 'react'; 
 import HyperViewLogo from '../assets/HyperViewLogo.png';
 import Footer from '../Components/Footer.jsx';
+import Swal from 'sweetalert2';
+import { Send_Message } from '../../Service_Client.js';
 
 function Contact_Us(){
+    const [Email, setEmail] = useState();
+    const [FullName, setFullName] = useState();
+    const [Message, setMessage] = useState();
+
+    const handleSendMessage = async () => {
+        const response = await Send_Message(Email, FullName, Message);
+
+        if(response.status === true){
+            console.log(response.message);
+
+            Swal.fire({
+                icon: "success",
+                title: "Message sent!",
+                text: "We'll be in contact as soon as possible.",
+                customClass: {
+                    confirmButton: "text-white bg-blue-500 font-bold rounded-3xl cursor-pointer w-full py-2 px-6 m-2 hover:bg-blue-400 focus:outline-2 focus:outline-offset-2 focus:outline-blue-500 active:bg-blue-600"
+                },
+                buttonsStyling: false
+            });
+
+            setEmail('');
+            setFullName('');
+            setMessage('');
+            document.querySelectorAll('input[type="text"]').forEach(input => input.value = '');
+            document.querySelectorAll('textarea').forEach(textarea => textarea.value = '');
+        }else{
+            Swal.fire({
+                icon: "error",
+                title: "Error!",
+                text: "Something went wrong, try again.",
+                customClass: {
+                    confirmButton: "text-white bg-blue-500 font-bold rounded-3xl cursor-pointer w-full py-2 px-6 m-2 hover:bg-blue-400 focus:outline-2 focus:outline-offset-2 focus:outline-blue-500 active:bg-blue-600"
+                },
+                buttonsStyling: false
+            });
+        }
+    }
+
+    function validateFields(){
+        const emailRegExp = new RegExp('^[\\w.-]+@([\\w-]+\\.)+[\\w]{2,4}$');
+        
+        if(!Email || !FullName || !Message){
+            Swal.fire({
+                icon: "error",
+                title: "Error!",
+                text: "Please fill out all fields.",
+                customClass: {
+                    confirmButton: "text-white bg-blue-500 font-bold rounded-3xl cursor-pointer w-full py-2 px-6 m-2 hover:bg-blue-400 focus:outline-2 focus:outline-offset-2 focus:outline-blue-500 active:bg-blue-600"
+                },
+                buttonsStyling: false
+            });
+            return;
+        }else{
+            if(!emailRegExp.test(Email)){
+                Swal.fire({
+                    icon: "error",
+                    title: "Error!",
+                    text: "Invalid email.",
+                    customClass: {
+                        confirmButton: "text-white bg-blue-500 font-bold rounded-3xl cursor-pointer w-full py-2 px-6 m-2 hover:bg-blue-400 focus:outline-2 focus:outline-offset-2 focus:outline-blue-500 active:bg-blue-600"
+                    },
+                    buttonsStyling: false
+                });
+                return;
+            }else{
+                handleSendMessage();
+            }
+        }
+    } 
+
     return(
         <>
           <div className='h-screen flex flex-col justify-between'>
@@ -16,21 +89,30 @@ function Contact_Us(){
                         <div className='my-5 w-full'>
                             <div className='my-4'>
                                 <h3 className='font-bold'>Full name</h3>
-                                <input type="text" placeholder='Full Name' className='w-full border-b-2 border-blue-500/70 p-1 focus:outline-0 focus:border-blue-500'/>
+                                <input type="text" placeholder='Full Name' 
+                                    onChange={e => setFullName(e.target.value)}
+                                    className='w-full border-b-2 border-blue-500/70 p-1 focus:outline-0 focus:border-blue-500'
+                                />
                             </div>
 
                             <div className='my-4'>
                                 <h3 className='font-bold'>Email</h3>
-                                <input type="text" placeholder='Email' className='w-full border-b-2 border-blue-500/70 p-1 focus:outline-0 focus:border-blue-500'/>
+                                <input type="text" placeholder='Email' 
+                                    onChange={e => setEmail(e.target.value)}
+                                    className='w-full border-b-2 border-blue-500/70 p-1 focus:outline-0 focus:border-blue-500'
+                                />
                             </div>
                             
                             <div className='my-4'>
                                 <h3 className='font-bold'>Message</h3>
-                                <textarea type="text" className='w-full border-b-2 border-blue-500/70 p-1 focus:outline-0 focus:border-blue-500'/>
+                                <textarea type="text" 
+                                    onChange={e => setMessage(e.target.value)}
+                                    className='w-full border-b-2 border-blue-500/70 p-1 focus:outline-0 focus:border-blue-500'
+                                />
                             </div>
                         </div>
 
-                        <button className="bg-blue-500 font-bold rounded-2xl cursor-pointer w-full py-2 m-2 hover:bg-blue-400 focus:outline-2 focus:outline-offset-2 focus:outline-blue-500 active:bg-blue-600 ">
+                        <button  onClick={validateFields} className="bg-blue-500 font-bold rounded-2xl cursor-pointer w-full py-2 m-2 hover:bg-blue-400 focus:outline-2 focus:outline-offset-2 focus:outline-blue-500 active:bg-blue-600 ">
                             Contact Us
                         </button>
                     </div>
