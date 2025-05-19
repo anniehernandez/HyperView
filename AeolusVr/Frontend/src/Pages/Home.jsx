942530e82b1c4daffcf045a972d47ec53072d11b
anniehernandez <sofia.hernandezslz@uanl.edu.mx>
2025-05-19 17:38:22 -0600

    Added right media assets

--- AeolusVr/Frontend/src/Pages/Home.jsx
+++ AeolusVr/Frontend/src/Pages/Home.jsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom';
 import HyperViewLogo from '../assets/HyperViewLogo.png';
 import AeolusVrLogo from '../assets/AeolusVrLogo.png';
-import Windows from '../assets/windows.jpg';
-import Rectangle from '../assets/Rectangle.png';
+import Simulation from "../assets/Simulation.webm";
+import Landscape from "../assets/Landscape.png";
+import Scenery from '../assets/Scenery.png';
+import Process from '../assets/Process.png';
 import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
 import EmojiObjectsOutlinedIcon from '@mui/icons-material/EmojiObjectsOutlined';
 import Header from '../Components/Header.jsx';
@@ -15,13 +17,21 @@ function Home(){
 
             <div className='h-screen flex flex-col justify-between'>
                 <div 
-                    className="px-30 py-32 bg-blend-multiply bg-cover bg-center" 
-                    style={{ 
-                        backgroundImage: `linear-gradient(to bottom, rgba(13, 42, 84, 0.8), rgba(13, 42, 84, 0) 100%), url(${Windows})` 
-                    }}
+                    className="relative px-30 py-32 bg-blend-multiply bg-cover bg-center overflow-hidden"
                 >
-                    <h3 className="text-4xl font-semibold">Technology for the real world.</h3>
-                    <img src={HyperViewLogo} alt="Logo" className="w-1/12 m-2"/>
+                    <video className="absolute top-0 left-0 w-full h-full object-cover z-0" autoPlay loop muted playsInline >
+                        <source src={Simulation} type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+
+                    <div className="absolute top-0 left-0 w-full h-full z-10" style={{
+                        background: 'linear-gradient(to bottom, rgba(13, 42, 84, 0.8), rgba(13, 42, 84, 0) 100%)'
+                    }} />
+
+                    <div className="relative z-20">
+                        <h3 className="text-4xl font-semibold">Technology for the real world.</h3>
+                        <img src={HyperViewLogo} alt="Logo" className="w-1/12 m-2"/>
+                    </div>
                 </div>
 
                 <div className="flex flex-row justify-center items-center h-full">
@@ -36,11 +46,11 @@ function Home(){
                                 specialized training, and rehabilitation in various areas.
                             </p>
 
-                            <Link to="/About_Us">
-                                <button className="bg-blue-950 font-bold rounded-2xl py-2 px-5 cursor-pointer hover:bg-blue-900 focus:outline-2 focus:outline-offset-2 focus:outline-blue-950 active:bg-[var(--background-color)]">Learn More <ArrowForwardIosRoundedIcon/></button>
+                            <Link to="/About_Us" className="bg-blue-950 font-bold rounded-2xl py-2 px-5 cursor-pointer hover:bg-blue-900 focus:outline-2 focus:outline-offset-2 focus:outline-blue-950 active:bg-[var(--background-color)]"> 
+                                Learn More <ArrowForwardIosRoundedIcon/>
                             </Link>
                         </div>
-                        <img src={Windows} alt="" className="max-w-4/10 object-cover rounded-tr-[3vw]"/>
+                        <img src={Scenery} alt="" className="max-w-4/10 max-h-120 object-cover rounded-tr-[3vw]"/>
                     </div>
                 </div>
             </div>
@@ -49,7 +59,7 @@ function Home(){
                 <div 
                     className="bg-[var(--background-color)]/50 bg-blend-multiply bg-cover bg-center py-12" 
                     style={{ 
-                        backgroundImage: `radial-gradient(circle, rgba(13, 42, 84, 0), rgba(13, 42, 84, 0.8)), url(${Windows})` 
+                        backgroundImage: `radial-gradient(circle, rgba(13, 42, 84, 0), rgba(13, 42, 84, 0.8)), url(${Landscape})` 
                     }}
                 >
                     <h2 className='uppercase semi-bold bg-blue-950 w-full px-30 py-3'><EmojiObjectsOutlinedIcon className='mr-2'/>New Project</h2>
@@ -66,17 +76,15 @@ function Home(){
                                 Learn more about our latest project! From its origins as a safe alternative for hang-gliding flight training, to the 
                                 development process of this technology and the results we have achieved.
                             </p>
-                            <Link to="/Projects/Aeolus_Vr">
-                                <button className="bg-blue-500 font-bold rounded-2xl py-2 px-5 cursor-pointer hover:bg-blue-400 focus:outline-2 focus:outline-offset-2 focus:outline-blue-500 active:bg-blue-600">
-                                    Learn More <ArrowForwardIosRoundedIcon/>
-                                </button>
+                            <Link to="/Projects/Aeolus_Vr" className='bg-blue-500 font-bold rounded-2xl py-2 px-5 cursor-pointer hover:bg-blue-400 focus:outline-2 focus:outline-offset-2 focus:outline-blue-500 active:bg-blue-600'>
+                                Learn More <ArrowForwardIosRoundedIcon/>
                             </Link>
                         </div>
                         <div className='flex flex-row justify-center items-center w-2/3'>
                             <div className='grid grid-cols-2 grid-rows-2 gap-5'>
-                                <img src={Rectangle} alt="" className='row-span-2 object-cover rounded-[2vw] h-full'/>
-                                <img src={Rectangle} alt="" className='col-span-1 object-cover rounded-[2vw]'/>
-                                <img src={Rectangle} alt="" className='col-span-1 object-cover rounded-[2vw]'/>
+                                <img src={Scenery} alt="" className='row-span-2 object-cover rounded-[2vw] h-full'/>
+                                <img src={Landscape} alt="" className='col-span-1 object-cover rounded-[2vw] h-full'/>
+                                <img src={Process} alt="" className='col-span-1 object-cover rounded-[2vw] h-full'/>
                                 
                             </div>
                         </div>
