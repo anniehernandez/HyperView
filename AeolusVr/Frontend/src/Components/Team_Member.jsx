3dd2999d0987e0d9a51cdcb3b0351ca7d6795947
anniehernandez <sofia.hernandezslz@uanl.edu.mx>
2025-05-19 17:35:40 -0600

    Modified Team_Member component desing

--- AeolusVr/Frontend/src/Components/Team_Member.jsx
+++ AeolusVr/Frontend/src/Components/Team_Member.jsx
@@ -2,13 +2,13 @@ import PermContactCalendarRoundedIcon from '@mui/icons-material/PermContactCalen
 function Team_Member({image, name, role, description, email}){
     return(
         <>
-            <div className='flex flex-row justify-center items-center bg-gray-200 p-6 rounded-[2vw]'>
-                <img src={image} alt="" className="mr-6 w-1/4 h-full object-cover"/>
+            <div className='flex flex-row justify-center items-center bg-gray-200 p-5 rounded-[2vw]'>
+                <img src={image} alt="" className="mr-6 w-1/4 h-full object-cover rounded-3xl"/>
                 <div className="flex flex-col">
                     <h2 className="text-lg text-blue-950 font-bold">{name}</h2>
                     <h3 className="text-blue-500 font-semibold m-1">{role}</h3> 
                     <p className="text-xs text-black">{description}</p>
-                    <h3 className='text-blue-950 font-semibold my-2'><PermContactCalendarRoundedIcon className='mr-2'/>{email}</h3>
+                    <h3 className='text-sm text-blue-950 font-semibold my-2'><PermContactCalendarRoundedIcon className='mr-2'/>{email}</h3>
                 </div>
             </div>
         </>
